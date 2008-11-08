bbd6a9c5a78df62ed11f69dec7f9ed4b21c99105
ashbb <ashbbb@gmail.com>
2008-11-08 13:08:04 +0900

    daylight saving time off

--- GangsClock2_v0.3f/make_yaml.rb
+++ GangsClock2_v0.3f/make_yaml.rb
@@ -1,35 +1,35 @@
 require 'yaml'
 
 data =<<-EOS
-Anita Kuno, -4, Canada, 0
-Brad Coish, -3, Canada, 0
+Anita Kuno, -5, Canada, 0
+Brad Coish, -4, Canada, 0
 Carlan Calazans, -2, Brazil, 3
-Chris Porter, +1, UK, 1
+Chris Porter, 0, UK, 1
 Christopher Bailey, -6, USA, 5
 Courtney House, -8, USA, 0
-Daniel Kotowski, +2, Poland, 2
+Daniel Kotowski, +1, Poland, 2
 Felipe Ureta, -6, Ecuador, 4
 George Thompson, -6, USA, 3
 Jeff Hales, -6, USA, 1
-Jerry Anning, -4, USA, 1
+Jerry Anning, -5, USA, 1
 Jim Pryke, -6, Canada, 2
-Jose Carlos Monteiro, +1, Portugal, 0
-Krzysztof Wicher, +1, UK, 2
+Jose Carlos Monteiro, 0, Portugal, 0
+Krzysztof Wicher, 0, UK, 2
 Leticia Figueira, -2, Brazil, 2
 Marcos Ricardo, -2, Brazil, 0
-Mareike Hybsier, +2, Germany, 1
-Michael Uplawski, +2, Germany, 0
-Michele Garoche, +2, France, 4
-Peter Crawford, +2, Italy, 3
+Mareike Hybsier, +1, Germany, 1
+Michael Uplawski, +1, Germany, 0
+Michele Garoche, +1, France, 4
+Peter Crawford, +1, Italy, 3
 Raul Parolari, -8, USA, 1
 Roy Stannard, +7, Thailand, 0
 Satish Talim, +5.5, India, 0
 Satoshi Asakawa, +9, Japan, 0
-Sergio Silva, +1, Portugal, 3
+Sergio Silva, 0, Portugal, 3
 Takaaki Kato, +9, Japan, 1
 Victor Goff, -6, USA, 0
 Willian Molinari, -2, Brazil, 1
-utc, 0, UTC/GMT, 0
+utc, 0, UTC/GMT, 5
 EOS
 
 Gang = Struct.new :name, :utc_offset, :country, :n, :avatar
