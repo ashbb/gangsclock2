8dff1ded087ce28b6b03c4d954b8e9ae8237c1fc
ashbb <ashbbb@gmail.com>
2008-11-30 15:52:00 +0900

    corrected some local timezone data

--- GangsClock2_v0.3f/make_yaml.rb
+++ GangsClock2_v0.3f/make_yaml.rb
@@ -6,11 +6,11 @@ Anita Kuno, -5, Canada, 0
 Brad Coish, -4, Canada, 0
 Carlan Calazans, -2, Brazil, 3
 Chris Porter, 0, UK, 1
-Christopher Bailey, -6, USA, 5
+Christopher Bailey, -6, USA, 3
 Courtney House, -8, USA, 0
 Daniel Kotowski, +1, Poland, 2
 Felipe Ureta, -6, Ecuador, 4
-George Thompson, -6, USA, 3
+George Thompson, -6, USA, 0
 Jeff Hales, -6, USA, 1
 Jerry Anning, -5, USA, 1
 Jim Pryke, -6, Canada, 2
@@ -31,7 +31,7 @@ Satish Talim, +5.5, India, 0
 Satoshi Asakawa, +9, Japan, 0
 Sergio Silva, 0, Portugal, 3
 Takaaki Kato, +9, Japan, 1
-Victor Goff, -6, USA, 0
+Victor Goff, -7, USA, 0
 Willian Molinari, -2, Brazil, 1
 EOS
 
