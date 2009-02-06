33cf14d198df143750f9a8f0955885574de76d17
ashbb <ashbbb@gmail.com>
2009-02-07 00:47:03 +0900

    Welcome Sami!

--- GangsClock2_v0.3g/make_yaml.rb
+++ GangsClock2_v0.3g/make_yaml.rb
@@ -1,11 +1,10 @@
 require 'yaml'
 
 data =<<-EOS
-utc, 0, UTC/GMT, 4
 Anita Kuno, -5, Canada, 0
 Brad Coish, -4, Canada, 0
 Carlan Calazans, -2, Brazil, 3
-Chris Porter, 0, UK, 1
+Chris Porter, 0, UK, 4
 Christopher Bailey, -6, USA, 2
 Courtney House, -8, USA, 0
 Daniel Kotowski, +1, Poland, 2
@@ -19,7 +18,7 @@ Jeff Hales, -6, USA, 5
 Jerry Anning, -5, USA, 1
 Jim Pryke, -6, Canada, 1
 Jose Carlos Monteiro, 0, Portugal, 0
-Krzysztof Wicher, 0, UK, 2
+Krzysztof Wicher, 0, UK, 1
 Leticia Figueira, -2, Brazil, 2
 Lowell Vizon, -8, USA, 2
 Marcos Ricardo, -2, Brazil, 0
@@ -32,6 +31,7 @@ Peter Crawford, +1, Italy, 3
 Raul Parolari, -8, USA, 1
 Roy Stannard, +7, Thailand, 0
 Ryan Fraley, -5, USA, 2
+Sami Ahmed, 0, UK, 2
 Satish Talim, +5.5, India, 0
 Satoshi Asakawa, +9, Japan, 0
 Sergio Silva, 0, Portugal, 3
